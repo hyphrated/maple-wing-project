ca686553760d8bd1e7f9b356daace76f3a910869
hyphrated <github@kryoblitz.com>
2026-08-21 09:45:47 -0700

    condense

---
+++ model.py
@@ -0,0 +1,24 @@
+import subprocess
+from ollama import chat
+
+def get_vram_gb():
+    result = subprocess.check_output([
+        "nvidia-smi",
+        "--query-gpu=memory.total",
+        "--format=csv,noheader,nounits"
+    ])
+
+    vram_mb = int(result.decode().strip().splitlines()[0])
+    return vram_mb / 1024
+
+def model_choice():
+    vram_amount = get_vram_gb()
+    if vram_amount >= 8:
+        return 'qwen3:8b'
+    elif vram_amount >= 6:
+        return 'qwen3:4b'
+    else:
+        return 'qwen3:1.7b' 
+
+print(get_vram_gb())
+print(model_choice())
