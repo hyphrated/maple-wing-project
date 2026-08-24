9688ec174afc15f1372c2c2d5d7f83440b3f2312
gerald <gerald147858@gmail.com>
2026-08-24 13:54:14 -0700

    tweaked vram to match mines--configurable

--- model_selection.py
+++ model_selection.py
@@ -13,7 +13,7 @@ def get_vram_gb():
     return vram_mb / 1024
 def model_choice():
     vram_amount = get_vram_gb()
-    if vram_amount >= 8:
+    if vram_amount >= 7.9:
         return 'qwen3:8b'
     elif vram_amount >= 6:
         return 'qwen3:4b'
